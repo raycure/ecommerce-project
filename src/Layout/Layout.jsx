b0c81e0746fb0354f5e06fb68c41313544c34c32
betuldanismaz <danismazbetul3@gmail.com>
2024-12-24 17:32:38 +0300

    Footer and contact page

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import Footer from './Footer';
-import { Outlet } from 'react-router';
-import ToTopArrow from '../components/UI/ToTopArrow';
-import CustomNavbar from './CustomNavbar';
+// Layout.jsx
+import React from "react";
+import Footer from "./Footer";
+import { Outlet } from "react-router";
+import ToTopArrow from "../components/UI/ToTopArrow";
+import CustomNavbar from "./CustomNavbar";
+import "./Layout.css"; // Yeni CSS dosyasını import ediyoruz
 
 function Layout() {
-	return (
-		<>
-			<CustomNavbar />
-			<Outlet />
-			<Footer />
-			<ToTopArrow />
-		</>
-	);
+  return (
+    <div className="layout-container">
+      <CustomNavbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+      <ToTopArrow />
+    </div>
+  );
 }
+
 export default Layout;
